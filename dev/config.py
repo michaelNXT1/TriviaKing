c799071c62f3f724f6474d445441a00dea3aa086
Michael Tzahi <71826961+michaelNXT1@users.noreply.github.com>
2024-04-01 18:07:46 +0300

    Expand config.py

--- dev/config.py
+++ dev/config.py
@@ -2,7 +2,13 @@ general = dict(
     buffer_size=1024
 )
 
-client_constants = dict(
+server_consts = dict(
+    server_port=13117,
+    magic_cookie=0xabcddcba,
+    message_type=0x02
+)
+
+client_consts = dict(
     server_port=13117,
     magic_cookie=0xabcddcba,
     message_type=0x02
@@ -14,5 +20,20 @@ client_op_codes = dict(
 )
 
 server_op_codes = dict(
-    server_sends_welcome=0x00,
+    server_sends_message=0x00,
+    server_requests_input=0x01,
+    server_ends_game=0x02,
 )
+
+answer_keys = {
+    'Y': True,
+    'T': True,
+    '1': True,
+    'N': False,
+    'F': False,
+    '2': False
+}
+
+
+def game_welcome_message(server_name, subject):
+    return f"Welcome to the {server_name} server, where we are answering trivia questions about {subject}."
